295dc743bd6d54ab1dd72079618fcda1ad4963c1
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-01-28 16:58:53 +0200

    Add good validation

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -6,7 +6,12 @@ const userSchema = Yup.object().shape({
   name: Yup.string()
     .min(3, "Name must be at least 3 symb long")
     .required("This is a required field"),
-  number: Yup.string().required("This is a required field"),
+  number: Yup.string()
+    .matches(
+      /^\d{2}-\d{3}-\d{2}-\d{2}$/,
+      "Phone number must be in xx-xxx-xx-xx format"
+    )
+    .required("This is a required field"),
 });
 
 export const ContactForm = ({ onAdd }) => {
