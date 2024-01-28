164892467ad03b08531bb543ebbf70340111b03c
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-01-28 17:17:39 +0200

    Now it`s all logic

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -7,10 +7,7 @@ const userSchema = Yup.object().shape({
     .min(3, "Name must be at least 3 symb long")
     .required("This is a required field"),
   number: Yup.string()
-    .matches(
-      /^\d{2}-\d{3}-\d{2}-\d{2}$/,
-      "Phone number must be in xx-xxx-xx-xx format"
-    )
+    .matches(/^\d{3}-\d{2}-\d{2}$/, "Phone number must be in xxx-xx-xx format")
     .required("This is a required field"),
 });
 
@@ -31,7 +28,7 @@ export const ContactForm = ({ onAdd }) => {
         actions.resetForm();
       }}
     >
-      <Form>
+      <Form autoComplete="off">
         <div>
           <label htmlFor={nameFieldId}>Username:</label>
           <Field type="text" name="name" id={nameFieldId} />
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,17 +1,10 @@
-// const accessNumber = {
-//   "459-12-56": "459-12-56",
-//   "443-89-12": "443-89-12",
-//   "645-17-79": "645-17-79",
-//   "227-91-26": "227-91-26",
-// };
-
 export const ContactList = ({ items, onDelete }) => {
   return (
     <ul>
       {items.map((item) => (
         <li key={item.id}>
           <p>{item.name}</p>
-          <p>+380 {item.number}</p>
+          <p>{item.number}</p>
           <button onClick={() => onDelete(item.id)}>Delete</button>
         </li>
       ))}
